cbfafe18248c4f27f02e008e820695e016913591
Defake <defake@yandex.ru>
2016-10-15 16:30:20 +0500

    kick off static casts

--- GameRPGpingpong/Character.cs
+++ GameRPGpingpong/Character.cs
@@ -14,7 +14,8 @@ namespace GameRPGpingpong
 		public readonly Stat defense;
 		public readonly Stat manna;
 		
-		public List<Skill> skills { get; }
+		public List<SkillTarget> targetSkills { get; }
+		public List<SkillAOE> aoeSkills { get; }
 
 		private readonly List<Stat> _stats; 
 
@@ -25,10 +26,12 @@ namespace GameRPGpingpong
 			_stats.Add(this.manaRegen = new Stat(manaRegen));
 			_stats.Add(this.defense = new Stat(defense));
 			_stats.Add(manna = new Stat(100));
-			skills = new List<Skill>();
+			targetSkills = new List<SkillTarget>();
+			aoeSkills = new List<SkillAOE>();
 		}
 
-		public void LearnNewSkill(Skill skill) => skills.Add(skill);
+		public void LearnNewSkill(SkillTarget skill) => targetSkills.Add(skill);
+		public void LearnNewSkill(SkillAOE skill) => aoeSkills.Add(skill);
 		
 		/* Here will be some player controls or AI system to cast skills */
 
--- GameRPGpingpong/SkillsFactory.cs
+++ GameRPGpingpong/SkillsFactory.cs
@@ -12,36 +12,39 @@ namespace GameRPGpingpong
 	{
 
 		// Positive skills
-		public static Skill StrengthOfGods() => 
+		public static SkillTarget StrengthOfGods() => 
 			new SkillTarget(50, 100, character => character.defense.AddModificator(new Modificator(+40, 40)));
 
-		public static Skill BulletEnergy() =>
+		public static SkillTarget BulletEnergy() =>
 			new SkillTarget(20, 15, character => character.speed.AddModificator(new Modificator(+50, 7)));
 
 		// Heals by 15 every second during 5 seconds. 75 at all
-		public static Skill TastyJuicefall() =>
+		public static SkillTarget TastyJuicefall() =>
 			new SkillTarget(45, 80, character => character.health.AddModificator(new Modificator(+15, 5, true)));
 
 		// Heals several characters by 20 during 4 sec.
-		public static Skill HealingWall() =>
+		public static SkillAOE HealingWall() =>
 			new SkillAOE(60, 120, character => character.health.AddModificator(new Modificator(+20, 4, true)));
 
 		// Negative skills
-		public static Skill ArmorBreak() =>
+		public static SkillTarget ArmorBreak() =>
 			new SkillTarget(40, 50, character => character.defense.AddModificator(new Modificator(-10, 30)));
 
-		public static Skill ManaBurn() =>
+		public static SkillTarget ManaBurn() =>
 			new SkillTarget(35, 20, character => character.manaRegen.AddModificator(new Modificator(-30, 15)));
 
 		// This skill will reduce character's mana by 8 every second during 10 seconds. 80 mana at all
-		public static Skill ManaVoid() =>
+		public static SkillTarget ManaVoid() =>
 			new SkillTarget(35, 140, character => character.manna.AddModificator(new Modificator(-8, 10, true)));
 
 		// This AOE skill will sets all characters' skills to cooldown
-		public static Skill ElectricShock() =>
+		public static SkillAOE ElectricShock() =>
 			new SkillAOE(50, 80, character =>
 			{
-				foreach (Skill skill in character.skills)
+				foreach (SkillAOE skill in character.aoeSkills)
+					skill.SetToCooldown();
+
+				foreach (SkillTarget skill in character.targetSkills)
 					skill.SetToCooldown();
 			});
 
